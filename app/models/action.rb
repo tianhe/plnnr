94b90c81279b1799d63f7d2e11e011103c4609bc
Tian He <tianhe@gmail.com>
2014-05-21 21:22:16 -0400

    update actions

--- app/models/action.rb
+++ app/models/action.rb
@@ -2,7 +2,8 @@ class Action < ActiveRecord::Base
   validates :position,    presence: true
   validates :description, presence: true
   validates :task_id,     presence: true
-
+  validates :name,        presence: true
+  
   belongs_to :task
 #  acts_as_list scope: :task
 
--- app/models/main_action.rb
+++ app/models/main_action.rb
@@ -1,5 +1,6 @@
 class MainAction < Action
   belongs_to :task
+  has_many :feedback_actions
 #  acts_as_list scope: :task
 
   has_many :providers, through: :action_providers
