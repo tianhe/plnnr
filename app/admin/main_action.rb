029cbcf95a4be63ee9c479919183ddbf60bf0742
Tian He <tianhe@gmail.com>
2014-05-21 21:14:00 -0400

    update permitted actions

--- app/admin/main_action.rb
+++ app/admin/main_action.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register MainAction do
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :task_id, :name, :description, :position
+  permit_params :task_id, :name, :description, :parent_action_id, :triggering_condition, :position
   #
   # or
   #
