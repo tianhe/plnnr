3c2d14f3a5e2f76bf77086e4a5123faee325ef31
Tian He <tianhe@gmail.com>
2014-06-11 22:51:35 -0400

    update model

--- app/models/feedback_action.rb
+++ app/models/feedback_action.rb
@@ -1,13 +1,2 @@
-class FeedbackAction < ActiveRecord::Base
-  validates :position,    presence: true
-  validates :description, presence: true
-  validates :task_id,     presence: true
-  validates :name,        presence: true
-  
-  belongs_to :task
-#  acts_as_list scope: :task
-  
-  has_many :feedbacks    
-  has_many :providers, through: :action_providers
-  has_many :action_providers
+class FeedbackAction < Action
 end
--- app/models/main_action.rb
+++ app/models/main_action.rb
@@ -1,13 +1,2 @@
-class MainAction < ActiveRecord::Base
-  validates :position,    presence: true
-  validates :description, presence: true
-  validates :task_id,     presence: true
-  validates :name,        presence: true
-  
-  belongs_to :task
-#  acts_as_list scope: :task
-  
-  has_many :feedbacks    
-  has_many :providers, through: :action_providers
-  has_many :action_providers
+class MainAction < Action
 end
