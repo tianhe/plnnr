9d19ac4aab3fcac591293c8719ebf2daf0e60614
Tian He <tianhe@gmail.com>
2014-06-11 23:43:04 -0400

    remove triggering condition migration

--- db/migrate/20140612112710_delete_triggering_condition_in_actions.rb
+++
@@ -1,5 +0,0 @@
-class DeleteTriggeringConditionInActions < ActiveRecord::Migration
-  def change
-    remove_column :actions, :type, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140612110910) do
+ActiveRecord::Schema.define(version: 20140612112710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
